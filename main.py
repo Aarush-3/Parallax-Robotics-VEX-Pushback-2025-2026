c79a3615489594a1e69b58a8f0af1bce6639a38a
Aarush-3 <aarushjp26@gmail.com>
2025-12-09 20:07:45 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,5 @@
 from vex import *
-
-brain = Brain()
-# create brain
-
-# --- Drivetrain Motors ---
-# This is for Port 3: Left side motor (False = not reversed)
-LeftMotor = Motor(Ports.PORT3, GearSetting.RATIO_18_1, False)
-# This is for Port 4: Right side motor (True = MUST be reversed for the robot to drive straight)
-RightMotor = Motor(Ports.PORT4, GearSetting.RATIO_18_1, True)
-
-
-Drivetrain = DriveTrain(LeftMotor, RightMotor, 12.56, 12.5, DistanceUnits.INCHES)
-
-ConveyorBelt = Motor(Ports.PORT1, GearSetting.RATIO_18_1, False)
-Intake = Motor(Ports.PORT2, GearSetting.RATIO_18_1, False)
-
-Controller1 = Controller(Primary.CONTROLLER)
-BumperSwitch = Bumper(Ports.PORTD)
+from robot-config import *
 
 Vision = Vision(PORT5) # Might want to check the port locations
 
